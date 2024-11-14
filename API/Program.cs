dc1d0267ece8409012e2750b84de8d5e95db9825
5laes <92576622+5laes@users.noreply.github.com>
2024-11-14 13:53:07 +0100

    Initiam Commit

---
+++ API/Program.cs
@@ -0,0 +1,26 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(x => 
+    x.UseSqlite(builder.Configuration.GetConnectionString("Default")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// Map controllers
+app.MapControllers();
+
+app.Run();
