42cb0ccde1555cb2ce90161e03eedbce5159bf38
5laes <92576622+5laes@users.noreply.github.com>
2024-11-21 13:57:05 +0100

    Error handling in the frontend using interceptors to catch http error and nicely display them using ngx-toastr

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -21,6 +21,7 @@ import { HomeModule } from './home/home.module';
     HomeModule
   ],
   providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
     provideClientHydration(),
     provideHttpClient(withInterceptorsFromDi())
   ],
