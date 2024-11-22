afca1e7c400c779cad595e65e2388ca61408bdb1
5laes <92576622+5laes@users.noreply.github.com>
2024-11-22 17:40:47 +0100

    Added basket functionality in the client you can now add edit and remove items to your basket used behaviorSubject and async pipe to subscribe and unsubscribe to observables

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -10,7 +10,18 @@ const routes: Routes = [
   { path: 'test-error', component: TestErrorComponent, data: {breadcrumb: "Test Errors"} },
   { path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: "Server Error"} },
   { path: 'not-found', component: NotFoundComponent, data: {breadcrumb: "Not Found"} },
-  { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: "Shop"} }, // this is lazy loading
+  {
+    path: 'shop', loadChildren: () => import('./shop/shop.module')
+      .then(mod => mod.ShopModule), data: { breadcrumb: "Shop" } // this is lazy loading
+  },
+  {
+    path: 'basket', loadChildren: () => import('./basket/basket.module')
+      .then(mod => mod.BasketModule), data: { breadcrumb: "Basket" } // this is lazy loading
+  },
+  {
+    path: 'checkout', loadChildren: () => import('./checkout/checkout.module') 
+      .then(mod => mod.CheckoutModule), data: { breadcrumb: "Checkout" } // this is lazy loading
+  },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
 
